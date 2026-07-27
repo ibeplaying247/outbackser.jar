c66ada169f35413af9f7ce364a854abeb7e7018d
ibeplaying247 <sootypage11@gmail.com>
2026-07-27 19:00:30 -0400

    Delete outback-forge-server directory

--- outback-forge-server/target/debug/deps/outback_branding-760eea8b4a7f5481.d
+++
@@ -1,7 +0,0 @@
-C:\Users\thego\OutbackInstaller\packages\servers\Outback\outback-forge-server\target\debug\deps\outback_branding-760eea8b4a7f5481.d: C:\Users\thego\OutbackInstaller\packages\servers\Outback\outback-branding\src\lib.rs
-
-C:\Users\thego\OutbackInstaller\packages\servers\Outback\outback-forge-server\target\debug\deps\liboutback_branding-760eea8b4a7f5481.rlib: C:\Users\thego\OutbackInstaller\packages\servers\Outback\outback-branding\src\lib.rs
-
-C:\Users\thego\OutbackInstaller\packages\servers\Outback\outback-forge-server\target\debug\deps\liboutback_branding-760eea8b4a7f5481.rmeta: C:\Users\thego\OutbackInstaller\packages\servers\Outback\outback-branding\src\lib.rs
-
-C:\Users\thego\OutbackInstaller\packages\servers\Outback\outback-branding\src\lib.rs:
--- outback-forge-server/target/debug/deps/outback_forge_server.d
+++
@@ -1,5 +0,0 @@
-C:\Users\thego\OutbackInstaller\packages\servers\Outback\outback-forge-server\target\debug\deps\outback_forge_server.d: src\main.rs
-
-C:\Users\thego\OutbackInstaller\packages\servers\Outback\outback-forge-server\target\debug\deps\outback_forge_server.exe: src\main.rs
-
-src\main.rs:
