2ac243942ba9b021c759fe4311d95935a12772c1
ibeplaying247 <sootypage11@gmail.com>
2026-07-27 19:01:03 -0400

    Delete outback-mod-api directory

--- outback-mod-api/target/debug/deps/outback_mod_api-a09b86e6da4a104a.d
+++
@@ -1,5 +0,0 @@
-C:\Users\thego\OutbackInstaller\packages\servers\Outback\outback-mod-api\target\debug\deps\outback_mod_api-a09b86e6da4a104a.d: src\lib.rs
-
-C:\Users\thego\OutbackInstaller\packages\servers\Outback\outback-mod-api\target\debug\deps\liboutback_mod_api-a09b86e6da4a104a.rmeta: src\lib.rs
-
-src\lib.rs:
