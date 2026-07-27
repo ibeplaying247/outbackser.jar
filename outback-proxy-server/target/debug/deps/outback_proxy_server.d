871ebc47623a4f3e3b663b81ee6b7682af1f568e
ibeplaying247 <sootypage11@gmail.com>
2026-07-27 19:01:50 -0400

    Delete outback-proxy-server directory

--- outback-proxy-server/target/debug/deps/outback_proxy_server.d
+++
@@ -1,5 +0,0 @@
-C:\Users\thego\OutbackInstaller\packages\servers\Outback\outback-proxy-server\target\debug\deps\outback_proxy_server.d: src\main.rs
-
-C:\Users\thego\OutbackInstaller\packages\servers\Outback\outback-proxy-server\target\debug\deps\outback_proxy_server.exe: src\main.rs
-
-src\main.rs:
