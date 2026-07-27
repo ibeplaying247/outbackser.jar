7539b4c9797e052eaf63c4b0720227b24e5028f6
ibeplaying247 <sootypage11@gmail.com>
2026-07-27 19:01:29 -0400

    Delete outback-paper-server directory

--- outback-paper-server/target/debug/deps/outback_loader-287fb8fb51f7e858.d
+++
@@ -1,7 +0,0 @@
-C:\Users\thego\OutbackInstaller\packages\servers\Outback\outback-paper-server\target\debug\deps\outback_loader-287fb8fb51f7e858.d: C:\Users\thego\OutbackInstaller\packages\servers\Outback\outback-loader\src\lib.rs
-
-C:\Users\thego\OutbackInstaller\packages\servers\Outback\outback-paper-server\target\debug\deps\liboutback_loader-287fb8fb51f7e858.rlib: C:\Users\thego\OutbackInstaller\packages\servers\Outback\outback-loader\src\lib.rs
-
-C:\Users\thego\OutbackInstaller\packages\servers\Outback\outback-paper-server\target\debug\deps\liboutback_loader-287fb8fb51f7e858.rmeta: C:\Users\thego\OutbackInstaller\packages\servers\Outback\outback-loader\src\lib.rs
-
-C:\Users\thego\OutbackInstaller\packages\servers\Outback\outback-loader\src\lib.rs:
--- outback-paper-server/target/debug/deps/outback_paper_server.d
+++
@@ -1,5 +0,0 @@
-C:\Users\thego\OutbackInstaller\packages\servers\Outback\outback-paper-server\target\debug\deps\outback_paper_server.d: src\main.rs
-
-C:\Users\thego\OutbackInstaller\packages\servers\Outback\outback-paper-server\target\debug\deps\outback_paper_server.exe: src\main.rs
-
-src\main.rs:
