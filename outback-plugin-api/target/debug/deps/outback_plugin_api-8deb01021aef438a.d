b01674f5bea0ec6144121a060ff7c84596dcee0c
ibeplaying247 <sootypage11@gmail.com>
2026-07-27 19:01:39 -0400

    Delete outback-plugin-api directory

--- outback-plugin-api/target/debug/deps/outback_plugin_api-8deb01021aef438a.d
+++
@@ -1,5 +0,0 @@
-C:\Users\thego\OutbackInstaller\packages\servers\Outback\outback-plugin-api\target\debug\deps\outback_plugin_api-8deb01021aef438a.d: src\lib.rs
-
-C:\Users\thego\OutbackInstaller\packages\servers\Outback\outback-plugin-api\target\debug\deps\liboutback_plugin_api-8deb01021aef438a.rmeta: src\lib.rs
-
-src\lib.rs:
