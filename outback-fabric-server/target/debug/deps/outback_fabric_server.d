24d934512ebf992ce1c7a784dcdb716bc2e83de4
ibeplaying247 <sootypage11@gmail.com>
2026-07-27 19:00:14 -0400

    Delete outback-fabric-server directory

--- outback-fabric-server/target/debug/deps/outback_fabric_server.d
+++
@@ -1,5 +0,0 @@
-C:\Users\thego\OutbackInstaller\packages\servers\Outback\outback-fabric-server\target\debug\deps\outback_fabric_server.d: src\main.rs
-
-C:\Users\thego\OutbackInstaller\packages\servers\Outback\outback-fabric-server\target\debug\deps\outback_fabric_server.exe: src\main.rs
-
-src\main.rs:
